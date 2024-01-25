ef49b925eac2c12de9ea842ebc0205c0f79717d1
1dennispark <dennis.park@paust.io>
2024-01-25 13:37:42 +0900

    Update version to v0.0.4

--- packages/shaple/src/ShapleClient.ts
+++ packages/shaple/src/ShapleClient.ts
@@ -12,39 +12,39 @@ export class ShapleClient {
   public auth: GoTrueClient
 
   constructor(
-      protected shapleUrl: string,
-      protected shapleKey: string,
-      options?: ShapleClientOptions,
-    ) {
+    protected shapleUrl: string,
+    protected shapleKey: string,
+    options?: ShapleClientOptions
+  ) {
     if (!shapleUrl) throw Error('shapleUrl is required.')
     if (!shapleKey) throw Error('shapleKey is required.')
 
-    const authUrl = `${shapleUrl}/auth/v1`;
-    this.auth = this._initGoTrueClient(
-        authUrl,
-        options?.auth ?? {},
-        options?.global?.headers ?? {},
-    )
+    const authUrl = `${shapleUrl}/auth/v1`
+    this.auth = this._initGoTrueClient(authUrl, options?.auth ?? {}, options?.global?.headers ?? {})
   }
 
   private _initGoTrueClient(
-      authUrl: string,
-      options: GoTrueClientOptions,
-      headers?: Record<string, string>,
+    authUrl: string,
+    options: GoTrueClientOptions,
+    headers?: Record<string, string>
   ) {
     const authHeaders = {
       Authorization: `Bearer ${this.shapleKey}`,
       apikey: `${this.shapleKey}`,
-    };
+    }
 
     return new GoTrueClient({
-        ...options,
-        url: authUrl,
-        headers: { ...authHeaders, ...headers },
-    });
+      ...options,
+      url: authUrl,
+      headers: { ...authHeaders, ...headers },
+    })
   }
 }
 
-export function createClient(shapleUrl: string, shapleKey: string, options?: ShapleClientOptions): ShapleClient {
+export function createClient(
+  shapleUrl: string,
+  shapleKey: string,
+  options?: ShapleClientOptions
+): ShapleClient {
   return new ShapleClient(shapleUrl, shapleKey, options)
 }
